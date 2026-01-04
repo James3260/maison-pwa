b8cbbac531ae533c8f2b7a9d16795cd26e008bbb
James3260 <onenelshop@gmail.com>
2026-01-04 12:54:26 +0100

    Refactor install event listener for cache addition

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,7 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('maison-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './app.js'
-      ]);
-    })
+    caches.open('maison-cache').then(cache =>
+      cache.addAll(['./','index.html','style.css','app.js'])
+    )
   );
 });
