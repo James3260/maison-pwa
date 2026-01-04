40fd89bc72a0c3a7f2bf88b501322ed905fa4a1d
James3260 <onenelshop@gmail.com>
2026-01-04 12:57:22 +0100

    Modify go function to accept button parameter

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-function go(id) {
-  document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
-  document.querySelectorAll('.bottom-nav button').forEach(b => b.classList.remove('active'));
+function go(id, btn) {
+  document.querySelectorAll('.page').forEach(p =>
+    p.classList.remove('active')
+  );
+
+  document.querySelectorAll('.bottom-nav button').forEach(b =>
+    b.classList.remove('active')
+  );
 
   document.getElementById(id).classList.add('active');
-  event.currentTarget.classList.add('active');
+  btn.classList.add('active');
 }
